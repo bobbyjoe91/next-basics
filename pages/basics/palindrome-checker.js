ce8b7dc1e757390828555621efacd136eb6cc3df
bobbyjoe91 <bobby.cool00763@protonmail.com>
2023-01-01 12:43:24 +0700

    initialize palindrome checker page

---
+++ pages/basics/palindrome-checker.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+
+import styles from "../../styles/Home.module.css";
+
+export default function PalindromeChecker(props) {
+  return (
+    <>
+      <Head>
+        <title>Palindrome Checker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        
+      </main>
+    </>
+  );
+}
